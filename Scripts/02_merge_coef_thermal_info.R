754e42dd582b3517d93d1e960536368f3b33975c
jebyrnes <jarrett.byrnes@umb.edu>
2025-01-03 16:09:54 -0500

    add skeleton of todo

---
+++ Scripts/02_merge_coef_thermal_info.R
@@ -0,0 +1,5 @@
+#' -----------------------------------------------
+#' This script merges coefficients and the species
+#' thermal info generated from
+#' https://github.com/BAMcCollum/sp_thermal_limits
+#' -----------------------------------------------
