bdc18ed5aa948843da3171e45b0c03f0f0e37633
jebyrnes <jarrett.byrnes@umb.edu>
2025-01-03 16:12:05 -0500

    updating scripts capitalization

---
+++ scripts/01_calculate_coefficients.R
@@ -0,0 +1,6 @@
+#' -----------------------------------------------
+#' This script calculates the change coefficient
+#' for each species under an ordbeta regression
+#' and outputs a file with species names and 
+#' coefficients (and SEs)
+#' -----------------------------------------------
