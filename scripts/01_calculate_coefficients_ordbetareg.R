4609f461856fb84b5fa0e68348c2248d14f0b27c
jebyrnes <jarrett.byrnes@umb.edu>
2025-03-21 12:51:26 -0400

    update with fitted values

--- scripts/01_calculate_coefficients_ordbetareg.R
+++ scripts/01_calculate_coefficients_ordbetareg.R
@@ -14,12 +14,24 @@ library(tidyr)
 library(purrr)
 library(broom.mixed)
 library(broom)
-options()
+library(emmeans)
+
 
 # load the data
 source("scripts/load_data_and_provide_constants.R")
 
-coefs_long <- substrate_long |>
+# helper function
+get_fitted_values <- function(mod, adf){
+  emmeans(mod, 
+          specs = ~year_cent,
+          at = list(year_cent = 
+                      modelr::seq_range(adf$year_cent, 
+                                        200))) |>
+    tidy() |>
+    mutate(year = year_cent + mean(adf$year))
+}
+
+mods_long <- substrate_long |>
  # debug, try with only a few species
  # filter(species %in% c("scypha_sp", "botryllus_schlosseri")) |>
   
@@ -32,16 +44,27 @@ coefs_long <- substrate_long |>
                                       data = .x,
                                      file = glue::glue("abundance_models/{.x$sp1[1]}.rds"),
                                      file_refit = "on_change")), #.x is a placeholder for each nested df
-         coef = map(mod, ~tidy(.x) |> filter(term == "year_cent"))) |>
-  
+         coef = map(mod, ~tidy(.x) |> filter(term == "year_cent")),
+         fitted = map2(mod, data, get_fitted_values))
+
+coefs_long <- mods_long |>  
   # great, get back the data
   unnest(coef) |>
   select(-data, -mod, 
-         -effect, -component, -group) 
+         -effect, -component, -group, -fitted) 
 
 View(coefs_long)
 
 write_csv(coefs_long,"data/change_coefficients_ordbetareg.csv")
+
+# What about modeled predictions for figures marginalized
+# over site and area?
+fitted_long <- mods_long |>  
+  # great, get back the data
+  unnest(fitted) |>
+  select(-data, -mod, -coef) 
+
+write_csv(fitted_long,"data/fitted_long_ordbetareg.csv")
 
 #The species behaving badly with glmmTMB - so, let's try ordbetareg:
 
