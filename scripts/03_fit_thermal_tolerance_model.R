45ead1965e75bb30cbd9fa57b6d3ff9b6633f488
Breckie McCollum <bmccollum@berklee.edu>
2025-08-01 11:57:56 -0400

    updated figure

--- scripts/03_fit_thermal_tolerance_model.R
+++ scripts/03_fit_thermal_tolerance_model.R
@@ -14,7 +14,6 @@ library(performance)
 library(car)
 library(broom)
 library(ggrepel)
-library(gtsummary)
 library(gt)
 
 # load data and set themes
@@ -24,17 +23,30 @@ source("scripts/load_data_and_provide_constants.R")
 coefs_with_indicies <- read_csv("data/coefs_with_indices.csv")
 View (coefs_with_indicies)
 
+#Rename Halichondria (Halichondria) panicea to Halichondria panicea 
+coefs_with_indicies$gen_spp[coefs_with_indicies$gen_spp=="Halichondria (Halichondria) panicea"] <- "Halichondria panicea"
+
 ggplot(coefs_with_indicies, aes(BO21_tempmax_bdmean_mean, estimate))+
   geom_point()+
   stat_smooth(method = "lm") #Ideally would like to color by functional group, depth strata
 
 mod1 <- lm(estimate ~ BO21_tempmax_bdmean_mean, data = coefs_with_indicies)
 mod2 <- lm(estimate ~ BO21_tempmax_bdmin_mean, data = coefs_with_indicies)
+mod3 <- lm(estimate ~BO21_tempmax_bdmin_max, data = coefs_with_indicies)
 
+#Mod1
+check_model(mod1)
+check_normality(mod1) 
 
+anova(mod1)
+Anova(mod1)
+summary(mod1)
+tidy(mod1)
+glance(mod1)
+
+#Mod2
 check_model(mod2)
 check_normality(mod2) 
-
 
 anova(mod2)
 Anova(mod2)
@@ -42,13 +54,23 @@ summary(mod2)
 tidy(mod2)
 glance(mod2)
 
+#Mod3
+check_model(mod3)
+check_normality(mod3) #Non-normality of residuals detected (p = 0.028).
+
+anova(mod3)
+Anova(mod3)
+summary(mod3)
+tidy(mod3)
+glance(mod3)
+
 get_mod_info <- function(mod){
   mod |> 
     tidy() |> filter(term != "(Intercept)") |>
     bind_cols(glance(mod) |> select(r.squared))
 }
 
-mod_list <- list(mod1, mod2)
+mod_list <- list(mod1, mod2, mod3)
 layer_comp <- purrr:::map(mod_list,
             get_mod_info) |>
   bind_rows()
@@ -69,12 +91,12 @@ lm_eqn <- function(df){
 ggplot(coefs_with_indicies, aes(BO21_tempmax_bdmin_mean, estimate, label = gen_spp))+
   geom_point()+
   stat_smooth(method = "lm")+
-  geom_text_repel(size = 5)+
+  geom_text_repel(size = 6)+
   #annotate("text", x = 22.5, y = 0.2, label = lm_eqn(coefs_with_indicies), parse = TRUE, colour = "red")+
   labs(x = "Average Thermal Maxima (Occupancy derived max temp at species min depth) in (°C)",  
        y = "Coefficient of Change over 42 years",
        title ="Average Thermal Maxima and Change in Abundance")+
-  geom_hline(yintercept=0.0, linetype='dashed', colour='orange') +
-  geom_vline(xintercept=15, colour='green')
+  geom_hline(yintercept=0.0, linetype='dashed', linewidth = 1.5, colour='orange') +
+  geom_vline(xintercept=15, linewidth = 1.5, colour='green')
 
 ggsave("figures/coefs_with_indicies.jpg")
