01424603eb6ae8673cfe9284a2a7448f3e75c212
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2025-09-19 17:29:21 -0400

    update to get rare species

--- scripts/02_a_species_presence_when_where.r
+++ scripts/02_a_species_presence_when_where.r
@@ -6,9 +6,28 @@
 
 library(dplyr)
 library(readr)
-source("scripts/load_data_and_provide_constants.R")
+
+# go with raw data so unfiltered
+
+
+substrate <- 
+  read_csv("data/sebens_substrate_proportion_20250908.csv") |>
+  filter(year > 1970,
+         month %in% c(6:9),
+         angle == "vertical") |>
+  group_by(site, year) |>
+  slice_tail() |> # get second sample point if there are 2
+  ungroup()
 
 names(substrate)
+
+
+substrate_long <- substrate |>
+  
+  # pivot longer so attributes (i.e., species) are in rows  
+  tidyr::pivot_longer(c(alcyonium_sub : tubularia_sub),
+               names_to = "species",
+               values_to = "proportion")
 
 substrate_obs_count <- substrate_long |>
   group_by(species, area, depth_m) |>
