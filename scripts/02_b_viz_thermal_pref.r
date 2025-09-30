fddb235ca638a424ee5b0c54faabd30d90e91225
Breckie McCollum <bmccollum@berklee.edu>
2025-09-30 11:45:54 -0400

    removed unnecessary figure title

--- scripts/02_b_viz_thermal_pref.r
+++ scripts/02_b_viz_thermal_pref.r
@@ -1,10 +1,11 @@
+library(tidyverse)
 library(ggplot2)
 library(readr)
 
 # read in and go from here
 
 coefout <- read_csv("data/coefs_with_indices.csv")
-
+View(coefout)
 # plot data ---------------------------------------------------------------
 
 #color1 <- "#782391"
@@ -26,17 +27,17 @@ coefout %>%
                    y=BO21_tempmax_bdmean_q5,
                    yend=BO21_tempmax_bdmean_q95), color = color2, alpha=.5,
                position = position_nudge(x = 0.25)) +
-  annotate(geom="text",
-           x=3, y=30, 
-           hjust=-0.1, vjust=0.2,
-           label = 
-             "\n Max temp at mean depth",
-           color = color2,
-           fontface="bold",
-           size=5) +
+#  annotate(geom="text",
+#           x=3, y=30, 
+#           hjust=-0.1, vjust=0.2,
+#           label = 
+#             "\n Max temp at mean depth",
+#           color = color2,
+#           fontface="bold",
+#           size=5) +
   
   labs(x=NULL, y= expression(paste("Water Temperature in ", "\u00b0C"))) +
-  theme_bw() +
+  theme_classic() +
   theme(#plot.margin = margin(l=25,b=5,unit="pt"),
     axis.text.x = element_text(angle = -90, hjust = 0))
 
